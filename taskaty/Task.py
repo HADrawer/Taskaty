5b7533c762bca1102d8a364390c5a4e2171c2596
hashem <hashemalsaie0457@example.com>
2024-11-24 01:20:00 +0300

    dsa

--- taskaty/Task.py
+++ taskaty/Task.py
@@ -6,5 +6,5 @@ class Task:
         self.end_date = end_date
         self.done = done
         
-def __str__(self):
-    return f'{self.title}, {self.description}, {self.start_date}, {self.end_date}, {self.done}'
+    def __str__(self):
+        return f'{self.title}, {self.description}, {self.start_date}, {self.end_date}, {self.done}'
